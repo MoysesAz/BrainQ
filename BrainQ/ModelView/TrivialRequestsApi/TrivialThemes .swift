88c58befca629c68a320940d3bceaf210c5a1e9c
Moyses Azevedo <m.mirazev@gmail.com>
2022-10-14 15:51:30 -0300

    :sparkles: feat: Adicionando TrivialThemes

--- BrainQ/ModelView/TrivialRequestsApi/TrivialThemes .swift
+++ BrainQ/ModelView/TrivialRequestsApi/TrivialThemes .swift
@@ -7,5 +7,40 @@
 
 import Foundation
 
-enum TrivialThemes {
+enum TrivialThemes: String {
+    case film
+    case music
+    case nature
+    case computers
+    case mathematics
+    case mythology
+    case history
+    case animals
+    case comics
+    case animeAndManga
+
+    func returnNumber() -> Int {
+        switch self {
+        case .film:
+            return 11
+        case .music:
+            return 12
+        case .nature:
+            return 17
+        case .computers:
+            return 18
+        case .mathematics:
+            return 19
+        case .mythology:
+            return 20
+        case .history:
+            return 23
+        case .animals:
+            return 27
+        case .comics:
+            return 29
+        case .animeAndManga:
+            return 31
+        }
+    }
 }
